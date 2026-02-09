ad88a5b8754cfa3aa97bc46111bbeed3a486de0c
Aurélien Mora <ealrann@gmail.com>
2026-02-09 14:39:12 +0100

    refresh rate

--- crates/client/src/constants.rs
+++ crates/client/src/constants.rs
@@ -1,5 +1,5 @@
 pub const PROGRESS_BAR_STEPS: u64 = 1_000; // Report every 0.1%
-pub const TUI_REFRESH_HZ: u64 = 250;
+pub const TUI_REFRESH_HZ: u64 = 4;
 pub const TUI_REFRESH_INTERVAL_US: u64 = 1_000_000 / TUI_REFRESH_HZ;
 
 pub fn default_classgroup_element() -> [u8; 100] {
